4ff9282195150615d28dc1329f64157a5b521f7c
BrightkyEfoo <brightefoo@gmail.com>
2023-12-31 12:14:44 +0100

    layout validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,45 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NavBar from "@/components/navbar";
+"use client";
 import Footer from "@/components/footer";
 import GoWhatsappBtn from "@/components/goWhatsapp";
+import NavBar from "@/components/navbar";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { createContext, useState } from "react";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Suggest module",
-  description: "Powered by chillo.tech",
-};
+export const HomeContext = createContext({});
+
+
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [value, setValue] = useState({
+    metadata: {
+      title: "Suggest module",
+      description: "Powered by chillo.tech",
+    },
+    descriptions: [""],
+  });
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NavBar />
-        {children}
-        <Footer />
-        <GoWhatsappBtn />
-      </body>
+      <HomeContext.Provider value={value}>
+        <head>
+          <title>{value.metadata.title}</title>
+          <meta name="description" content={value.metadata.description} />
+        </head>
+        <body className={inter.className}>
+          <NavBar />
+          {children}
+          <Footer />
+          <GoWhatsappBtn />
+        </body>
+      </HomeContext.Provider>
     </html>
   );
 }

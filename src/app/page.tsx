99b53a8e56a3749bbb4902494a0291642ac32cf5
Achille <achille.mbougueng@chillo.tech>
2024-01-06 10:37:51 +0100

    projet initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-export default function Home() {
+function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     </main>
   )
 }
+
+export default Home;

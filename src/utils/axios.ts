370b63816126b4f0cecd882bd996c051026488b8
Achille <achille.mbougueng@chillo.tech>
2024-01-12 07:49:50 +0100

    configurations endpoints

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,10 @@ import axios, {AxiosError, AxiosRequestConfig} from "axios";
 
 const BASE_PATH = "api/v1"
 const onRequest = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => { 
-
+  console.log('====================================');
+  console.log(process.env)
+  console.log('====================================');
+  
   const baseURL = `${process.env.NEXT_PUBLIC_BACKEND_API}/${BASE_PATH}`;
   const authorization = { 'Authorization':`Bearer ${process.env.NEXT_PUBLIC_BACKEND_API_TOKEN}`};
 

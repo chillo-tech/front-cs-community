1ba884ad04a90828ee60664b920dcec4e78e97d9
Bright Efoo <brightefoo@gmail.com>
2024-02-21 16:24:08 +0100

    first integration of waiting-list page

---
+++ src/app/(wrapper)/waiting-list/[slug]/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import {
+  Analytics,
+  AvisFormInputWrapper,
+  CustomCheckbox,
+  Message,
+  SubmitButton,
+} from "@/components";
+import { ScaleLoader } from "react-spinners";
+import { useWaitingList } from "./useWaitingList";
+
+const Home = () => {
+  const {
+    register,
+    onSubmit,
+    errors,
+    mutation,
+    viewQuery,
+    resetAll,
+    reloadPage,
+  } = useWaitingList();
+  return (
+    <>
+      <div className="container px-5 py-5 ">
+        {(viewQuery.status === "loading" || viewQuery.status === "error") && (
+          <div
+            className={
+              " bg-slate-200 py-3 px-3 md:px-10 md:text-lg rounded-md text-blue-900"
+            }
+          >
+            {viewQuery.status === "loading" ? (
+              <div className="flex top-0 left-0 z-50 justify-center items-center h-full w-full">
+                <ScaleLoader color="rgb(30,50,138)" />
+              </div>
+            ) : (
+              <Message
+                isError={true}
+                isSuccess={false}
+                reloadForm={reloadPage}
+                reloadText="Recharger la page"
+              />
+            )}
+          </div>
+        )}
+
+        {viewQuery.status === "success" && (
+          <form
+            className={
+              "flex relative flex-col gap-2 font-light infos my-3 bg-slate-200 py-3 rounded-mdd px-3 md:px-10 md:text-lg rounded-md text-blue-900"
+            }
+            onSubmit={onSubmit}
+          >
+            {mutation.isError || mutation.isSuccess ? (
+              <Message
+                reloadForm={resetAll}
+                isError={mutation.isError}
+                isSuccess={mutation.isSuccess}
+              />
+            ) : (
+              <>
+                {mutation.isLoading ? (
+                  <div className="flex absolute top-0 left-0 bg-[rgba(30,50,138,.3)] z-50 justify-center items-center h-full w-full">
+                    <ScaleLoader color="rgb(30,50,138)" />
+                  </div>
+                ) : null}
+                <div className="">
+                  <p className="mb-2 font-black text-2xl mt-2">
+                    Merci de nous donner votre avis
+                  </p>
+                  <p>
+                    Les évaluations nous permmettent de constament nous
+                    améliorer. Globalement vous êtes ...
+                  </p>
+                </div>
+
+                {/* nom */}
+                <div className="flex flex-col">
+                  <label>Votre nom</label>
+                  <input
+                    className="py-1 px-2 text-black rounded-md"
+                    type="text"
+                    {...register("name")}
+                  />
+                  <p className="text-rose-800 text-sm">
+                    {errors && errors.name && "Veuillez entrer votre nom"}
+                  </p>
+                </div>
+
+                {/* email */}
+                <div className="flex flex-col">
+                  <label>Votre email</label>
+                  <input
+                    className="py-1 px-2 text-black rounded-md"
+                    type="email"
+                    {...register("email")}
+                  />
+                  <p className="text-rose-800 text-sm">
+                    {errors && errors.email && "Veuillez entrer votre email"}
+                  </p>
+                </div>
+
+                <div className="flex flex-col text-xl my-3">
+                  <SubmitButton
+                    text={"Rejoignez la liste d'attente".toUpperCase()}
+                  />
+                </div>
+                <p className="text-center">
+                  Nous respectons votre vie privée. Désabonnez-vous à tout
+                  moment.
+                </p>
+              </>
+            )}
+          </form>
+        )}
+      </div>
+      {process.env.NEXT_PUBLIC_AVIS_ANALYTICS && (
+        <Analytics ga_id={process.env.NEXT_PUBLIC_AVIS_ANALYTICS} />
+      )}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/app/(wrapper)/waiting-list/[slug]/useWaitingList.ts
@@ -0,0 +1,80 @@
+"use client";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation, useQuery } from "react-query";
+import { ApplicationContext } from "@/app/(wrapper)/ApplicationContext";
+import { waitingListSchema } from "./wl-schema";
+
+export const useWaitingList = () => {
+  const router = useRouter();
+  const params = useParams();
+  const slug = params?.slug;
+  const { setData } = useContext(ApplicationContext);
+
+  const mutation = useMutation(subscribeToWaitingList);
+  const viewQuery = useQuery("waiting-list", getWaitingList);
+
+  async function getWaitingList() {
+    const response = await axios.get(
+      `/api/backend/waiting-list/video/?id=${slug}`
+    );
+    if (response.data.video)
+      setData({
+        leftComponent: {
+          description: `Inscrivez-vous dès maintenant sur la liste d'attente et bénéficiez d'une réduction exclusive EARLY-BIRD dès que le cours sera disponible.`,
+          title: response.data.video.titre || "Video de chillo.tech",
+        },
+        metaData: {
+          description: "Powered by chillo.tech",
+          title: "Liste d'attente",
+        },
+      });
+    return response.data.video;
+  }
+
+  async function subscribeToWaitingList(data: any) {
+    await axios.post("/api/backend/waiting-list/subscribe", data);
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(waitingListSchema),
+  });
+
+  const onSubmitHandler = (data: any) => {
+    mutation.mutateAsync({
+      videoId: slug,
+      ...data,
+    });
+  };
+
+  const resetAll = () => {
+    reset();
+    mutation.reset();
+  };
+
+  const onInvalid = (errors: any) => console.error(errors);
+
+  const onSubmit = handleSubmit(onSubmitHandler, onInvalid);
+
+  const reloadPage = () => {
+    router.refresh();
+  };
+  return {
+    register,
+    errors,
+    onSubmit,
+    mutation,
+    viewQuery,
+    resetAll,
+    reloadPage,
+  };
+};

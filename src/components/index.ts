5f843ff7c85fd5819694ce13a6d00b5bc5809f0a
Bright Efoo <brightefoo@gmail.com>
2024-03-26 21:09:09 +0100

    last fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,7 @@ export * from "./Header";
 export * from "./Message";
 export * from "./SubmitButton";
 export * from "./WhatsappButton";
-export * from "./custom-check-box";
+export * from "./customCheckBox";
 export * from "./SuggestionsView/SuggestionCard";
 export * from "./SuggestionsView/SuggestionsCardContainer";
 export * from "./AvisView";
